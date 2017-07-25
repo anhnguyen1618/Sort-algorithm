88355c2ec42f4d6eed8a4d0753470371e82a489d
super zozo <Anh.Nguyen3@metropolia.fi>
2017-07-26 00:23:02 +0300

    add heap sort

---
+++ heap.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	originalArr := []int{1, 3, 12, 2, 6, 8, 5, 4}
+	heapSort(originalArr)
+	fmt.Println(originalArr)
+
+}
+
+func heapSort(arr []int) {
+	for size := len(arr); size > 1; size-- {
+		createMaxHeapInRange(arr, size)
+
+		/*
+		*	Swap first and last element of the remaining heap everytime we decrement the size
+		* of the heap so that the first node of the heap is excluded from the next heap
+		 */
+
+		arr[0], arr[size-1] = arr[size-1], arr[0]
+	}
+}
+
+func createMaxHeapInRange(arr []int, size int) {
+	for i := int((size - 1) / 2); i >= 0; i-- {
+		maxHeapify(arr, i, size)
+	}
+}
+
+func maxHeapify(arr []int, index int, size int) {
+
+	leftIndex, rightIndex := index*2+1, index*2+2
+
+	largest, indexLargest := arr[index], index
+
+	if rightIndex < size && arr[rightIndex] > largest {
+		largest, indexLargest = arr[rightIndex], rightIndex
+	}
+
+	if leftIndex < size && arr[leftIndex] > largest {
+		largest, indexLargest = arr[leftIndex], leftIndex
+	}
+
+	if largest != arr[index] {
+		arr[index], arr[indexLargest] = largest, arr[index]
+		maxHeapify(arr, indexLargest, size)
+	}
+
+}
