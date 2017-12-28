1274f5a56483d54dabf1c37bbb494a059ddba702
super zozo <Anh.Nguyen3@metropolia.fi>
2017-12-29 01:56:25 +0200

    add depth first search

--- graph/graph.go
+++ graph/graph.go
@@ -28,7 +28,6 @@ func (graph *Graph) LoadData(fileName string) {
 		numbers := strings.Split(line, " ")
 
 		for i := len(numbers) - 1; i > 0; i-- {
-			// fmt.Println(numbers[i])
 			number, err := strconv.Atoi(numbers[i])
 			if err != nil {
 				fmt.Println(err)
@@ -50,4 +49,43 @@ func (graph *Graph) PrintGraph() {
 
 		fmt.Println(stringBuilder)
 	}
+}
+
+func checkIfNodeIsVisited(visited *[]int, node int) bool {
+	for _, visitedNode := range *visited {
+		if visitedNode == node {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (graph *Graph) DepthFirstSearch(from int, to int, visited *[]int) int {
+	if checkIfNodeIsVisited(visited, from) {
+		return -1
+	}
+
+	*visited = append(*visited, from)
+
+	currentNode := graph.Data[from]
+	if currentNode.IndexPointer == to {
+		fmt.Println(from)
+		return from
+	}
+
+	neighborNode := currentNode.NeighborNode
+
+	for neighborNode != nil {
+		predNode := graph.DepthFirstSearch(neighborNode.IndexPointer, to, visited)
+		if predNode != -1 {
+			fmt.Println(from)
+			return neighborNode.IndexPointer
+		}
+
+		neighborNode = currentNode.NeighborNode
+	}
+
+	return -1
+
 }
--- graph/main.go
+++ graph/main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	graph := &Graph{[16]*Node{}}
 	graph.LoadData("maze.grh")
 	graph.PrintGraph()
+	fmt.Println("------------------")
+	graph.DepthFirstSearch(0, 15, &[]int{})
+
+	// if predNode == -1 {
+	// 	fmt.Println("no path")
+	// } else {
+	// 	fmt.Println(predNode)
+	// }
 }
